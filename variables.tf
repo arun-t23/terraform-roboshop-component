5062df21edc4fc8916da9d70388b883812b430be
Arun T <arunt98>
2025-11-16 13:48:39 -0500

    terraform

--- variables.tf
+++ variables.tf
@@ -1,13 +1,12 @@
 variable "project_name" {
-    type    =   string
-    default = "roboshop"
+  type        = string
+  default     = "roboshop"
 }
 
 variable "environment" {
-    type    =   string
-    default = "dev"
+  type        = string
+  default     = "dev"
 }
-
 
 variable "component" {
 
@@ -17,9 +16,6 @@ variable "rule_priority" {
 
 }
 
-variable "zone_id" {
-    default =   "Z101103636O9J5N20O0OE"
-}
 variable "domain_name" {
-    default =   "cloudquestdev.com"
+    default = "cloudquestdev.com"
 }
